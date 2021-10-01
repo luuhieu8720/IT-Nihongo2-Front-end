bd99a8939bd370c57ef063e7c881fc7591db6b67
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-01 22:42:02 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './App.css';
+
+function App() {
+	return (
+        <BrowserRouter>
+            <Switch>
+            	{/*<Route exact path="/" component={ }></Route>*/}
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default App;
