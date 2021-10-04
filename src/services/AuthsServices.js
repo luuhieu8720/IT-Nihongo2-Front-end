0bf2d7f84d75cba17c869aa70c9e6e1116f6b20d
hien25 <tronghienne@gmail.com>
2021-10-04 21:41:46 +0700

    Add sign-up feature

--- src/services/AuthsServices.js
+++ src/services/AuthsServices.js
@@ -10,6 +10,9 @@ class AuthsServices {
   reset(email) {
     return axios.post(reset_URL, email);
   }
+  signup(signup) {
+    return axios.post(auths_URL, signup);
+  }
 }
 
 export default new AuthsServices();
