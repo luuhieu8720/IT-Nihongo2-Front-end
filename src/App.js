f2d1c93b6dc5d5ea444434aad153146a350fecdd
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-06 23:01:32 +0700

    Update homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Signin from "./pages/SignIn";
 import PassReset from "./pages/PassReset";
-<<<<<<< HEAD
+import Signup from "./pages/Signup";
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -11,26 +11,11 @@ function App() {
             <Switch>
                 <Route exact path="/signin" component={Signin}></Route>
                 <Route exact path="/reset-password" component={PassReset}></Route>
+                <Route exact path="/signup" component={Signup}></Route>
                 <Route exact path="/" component={HomePage}></Route>
             </Switch>
         </BrowserRouter>
     );
-=======
-import Signup from "./pages/Signup";
-//import HomePage from "./pages/HomePage";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/signin" component={Signin}></Route>
-        <Route exact path="/reset-password" component={PassReset}></Route>
-        <Route exact path="/signup" component={Signup}></Route>
-        {/* <Route exact path="/" component={HomePage}></Route> */}
-      </Switch>
-    </BrowserRouter>
-  );
->>>>>>> master
 }
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,83 @@
 import { Link } from "react-router-dom";
+import SideBar from "../components/Homepage/Sidebar";
+import { Card } from "react-bootstrap";
+import TopComponent from "../components/Homepage/TopComponent";
+import StudentSideBar from "../components/StudentSideBar";
 
 function HomePage() {
     return (
-        <div>
-            <div className="col-3">
-                <div class="icon-bar rounded">
-                    <label className="text-side-bar">ETO</label>
-                    <div>
-                        <Link className="" href="#"><i class="fa fa-desktop"></i></Link>
-                        <Link href="#"><i className="fa fa-folder-o"></i></Link>
-                        <Link href="#"><i className="fa fa-user-o"></i></Link>
-                        <Link href="#"><i className="fa fa-cog"></i></Link>
-                    </div>
-                    <Link href="#" className="mt-sm-6"><i className="fa fa-sign-out"></i></Link>
-                </div>
+        <div class="row">
+            <div className="col-sm-auto">
+                <SideBar className="position-abs" />
             </div>
-            <div className="col-5">
-                <div className="col-6">
-                    <div className="position-abs mt-5 ms-6">
-                        <h4 className="text-tutor">Easy</h4>
-                    </div>
-                    <div className="position-abs mt-6 ms-7">
-                        <h4 className="text-tutor">Tutor</h4>
-                    </div>
-                    <div className="position-abs mt-7 ms-8">
-                        <h4 className="text-tutor">Online</h4>
-                    </div>
-                    <div className="post-tutor">
-                        <h4 className="font-weight-bold">Title</h4>
-                    </div>
-                </div>
-                <div className="col-6">
-                </div>
-            </div>
-            <div className="col-4">
+            <div className="col-sm-define ms-4">
+                <TopComponent />
+                <div style={{ marginTop: '30px' }}>
+                    <Card className="card-tutor">
+                        <Card.Body>
+                            <div className="row">
+                                <div className="col-sm-6">
+                                    <Card.Title>Title: Finding math teacher</Card.Title>
+                                    <Card.Text>Description: Need a high school math teacher</Card.Text>
+                                    <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
+                                </div>
+                                <div className="col-sm-6">
+                                    <div class="dropdown">
+                                        <i class="fa fa-ellipsis-v" style={{ marginLeft: '95%' }} type="" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        </i>
+                                        <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
+                                            <li><Link class="dropdown-item" >View detail</Link></li>
+                                            <li><Link class="dropdown-item" >Report this post</Link></li>
+                                            <li><Link class="dropdown-item" >Manage</Link></li>
+                                        </ul>
+                                    </div>
+                                    <Card.Subtitle>Salary: 300$</Card.Subtitle>
+                                    <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
+                                    <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
+                                    <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
 
+                                </div>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <Card className="card-tutor">
+                    <Card.Body>
+                        <Card.Title>Title: Finding math teacher</Card.Title>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <Card.Text>Description: Need a high school math teacher</Card.Text>
+                                <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
+                            </div>
+                            <div className="col-sm-6">
+                                <Card.Subtitle>Salary: 300$</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className="card-tutor">
+                    <Card.Body>
+                        <Card.Title>Title: Finding math teacher</Card.Title>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <Card.Text>Description: Need a high school math teacher</Card.Text>
+                                <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
+                            </div>
+                            <div className="col-sm-6">
+                                <Card.Subtitle>Salary: 300$</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
+                                <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-sm-3 student-top-component" style={{ paddingLeft: '5%' }}>
+                <StudentSideBar />
             </div>
         </div>
     );
