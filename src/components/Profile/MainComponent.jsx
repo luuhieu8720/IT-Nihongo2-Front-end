f672fe96d28b835b251a8b02af0fb4b666b310fa
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-07 23:11:17 +0700

    Finish  user's profile page design

--- src/components/Profile/MainComponent.jsx
+++ src/components/Profile/MainComponent.jsx
@@ -9,9 +9,9 @@ function MainComponent() {
             <div className="row">
                 <div className="col-sm-3">
                     <h3 className="" style={{ fontSize: '28px', marginTop: '18%', color: 'darkviolet' }}>Profile</h3>
-                    <p style={{ color: 'rgba(0, 0, 0, 0.5)', width: '0px', height: '350px', left: '35px', border: '1px solid #8d8989', marginLeft: '80%' }}></p>
+                    <p className="position-abs" style={{ color: 'rgba(0, 0, 0, 0.5)', width: '0px', height: '400px', left: '35px', border: '1px solid #8d8989', marginLeft: '35%', top:'30%' }}></p>
                 </div>
-                <div className="col-sm-2" style={{ marginTop:'9%' }}>
+                <div className="col-sm-3" style={{ marginTop:'6%' }}>
                     <h5 className="py-1">ID </h5>
                     <h5 className="py-1">Phone </h5>
                     <h5 className="py-1">Email </h5>
@@ -21,7 +21,7 @@ function MainComponent() {
                     <h5 className="py-1">Location </h5>
                     <h5 className="py-1">Speciality </h5>
                 </div>
-                <div className="col-sm-auto text-name-profile" style={{ marginTop:'8%' }}>
+                <div className="col-sm-auto text-name-profile" style={{ marginTop:'5%' }}>
                     <h6 className="pb-2">9999</h6>
                     <h6 className="pb-2 pt-1">(+84)123456789 </h6>
                     <h6 className="pb-2 pt-1">luuhieu8720@gmail.com</h6>
--- src/components/Profile/TopProfileComponent.jsx
+++ src/components/Profile/TopProfileComponent.jsx
@@ -4,11 +4,21 @@ import { Image } from "react-bootstrap";
 function TopProfileComponent() {
     return (
         <div className="">
-            <div className="" style={{ marginTop: '1.5%', display:'inline-block' }}>
-                <Image src="Image/avatardefault.png" alt="image" roundedCircle ></Image>
+            <div className="" style={{ marginTop: '1.5%', display: 'inline-block' }}>
+                <Image src="/Image/avatardefault.png" alt="image" roundedCircle ></Image>
+                <div class="dropdown position-abs" style={{ top: '3%', right: '3%' }}>
+                    <i class="fa fa-ellipsis-h fa-2x" type="" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                    </i>
+                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
+                        <li><Link class="dropdown-item" >Finding tutor</Link></li>
+                        <li><Link class="dropdown-item" >Back to home page</Link></li>
+                        <li><Link class="dropdown-item" >Sign out</Link></li>
+                    </ul>
+                </div>
             </div>
             <span className="position-abs text-name-profile">Hieu Le</span>
-            <h3 style={{paddingTop:'3.5%'}}>Settings</h3>
+            <h3 style={{ paddingTop: '3.5%' }}>Settings</h3>
+            <Link to="/"><i className="position-abs fas fa-pen fa-lg" style={{ marginTop:'5%', right:'10%' }}></i></Link>
         </div>
 
     );
