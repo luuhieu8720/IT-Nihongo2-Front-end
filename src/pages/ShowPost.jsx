d9b500e83b4ba7f4c108c861417f12099d3741e4
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-22 14:57:45 +0700

    Add show-post

--- src/pages/ShowPost.jsx
+++ src/pages/ShowPost.jsx
@@ -1,47 +1,79 @@
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useHistory } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import validator from "validator";
+import { useEffect, useState } from "react";
+import PostServices from "../services/PostServices";
 
-function ShowPost() {
-  const history = useHistory();
-  const [state, setState] = useState({
+function ShowPost({ match }) {
+  // const history = useHistory();
+  // const [state, setState] = useState({
+  //   title: "",
+  //   salary: "",
+  //   time: "",
+  //   day: "",
+  //   location: "",
+  //   gender: "",
+  //   description: "",
+  // });
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (!validator.isDecimal(state.salary)) {
+  //     toast.warning("Your salary is not number");
+  //   } else if (
+  //     state.title === "" ||
+  //     state.time === "" ||
+  //     state.day === "" ||
+  //     state.location === "" ||
+  //     state.gender === "" ||
+  //     state.description === ""
+  //   ) {
+  //     toast.warning("All fields are not allowed to be null");
+  //     setTimeout(() => {
+  //       history.push("/post/show");
+  //     }, 5000);
+  //   } else toast.success("Ok");
+  // };
+  // const handleChange = (evt) => {
+  //   const val = evt.target.value;
+  //   setState({
+  //     ...state,
+  //     [evt.target.name]: val,
+  //   });
+  // };
+
+  var postId = match.params.id;
+
+  const [post, setPost] = useState({
     title: "",
     salary: "",
     time: "",
-    day: "",
     location: "",
-    gender: "",
-    description: "",
-  });
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!validator.isDecimal(state.salary)) {
-      toast.warning("Your salary is not number");
-    } else if (
-      state.title === "" ||
-      state.time === "" ||
-      state.day === "" ||
-      state.location === "" ||
-      state.gender === "" ||
-      state.description === ""
-    ) {
-      toast.warning("All fields are not allowed to be null");
-      setTimeout(() => {
-        history.push("/post/show");
-      }, 5000);
-    } else toast.success("Ok");
-  };
-  const handleChange = (evt) => {
-    const val = evt.target.value;
-    setState({
-      ...state,
-      [evt.target.name]: val,
-    });
-  };
+    male: "",
+    description: ""
+  })
+
+  const [gender, setGender] = useState();
+
+  useEffect(() => {
+    PostServices.getPostInformation(postId)
+      .then((response) => {
+        setPost(response.data.value);
+        if (post.male === "true") {
+          setGender("Male");
+        }
+        else {
+          setGender("Female")
+        }
+      })
+      .catch((e) => {
+        if (e.response && e.response.data) {
+          toast.error(e.response.data.value)
+        }
+      })
+  }, [])
 
   return (
     <div className="background1">
@@ -61,57 +93,51 @@ function ShowPost() {
               className="input-showpost text-black position-abs"
               placeholder="Finding math teacher..."
               name="title"
-              onChange={handleChange}
+              value={post.title}
               style={{ left: "6.5%" }}
+              disabled="true"
             />
             <InputText
               className="input-showpost text-black position-abs"
               placeholder="300"
               name="salary"
-              onChange={handleChange}
               style={{ left: "49.5%" }}
+              disabled="true"
             />
             <p className="special">/</p>
             <InputText
               className="input-showpost text-black text-center position-abs"
               placeholder="9:00-11:00"
               name="time"
-              onChange={handleChange}
+              value={post.time}
               style={{ left: "49.5%", top: "35%", width: "13%" }}
+              disabled="true"
             />
             <InputText
               className="input-showpost text-black position-abs"
-              placeholder="Mon, Wed, Sun"
               name="day"
-              onChange={handleChange}
               style={{ left: "66%", top: "35%", width: "21.6%" }}
+              disabled="true"
             />
             <p className="money">$</p>
           </div>
           <div className="row-cols-6">
-            <select
+            <InputText
               className="input-select"
               name="location"
               style={{ top: "52%" }}
-              onChange={handleChange}
-              value={state.value}
+              value={post.location}
+              disabled="true"
             >
-              <option value="DaNang">Da Nang</option>
-              <option value="QuangNam">Quang Nam</option>
-              <option value="QuangNgai">Quang Ngai</option>
-              <option value="Hue">Hue</option>
-            </select>
-            <select
+            </InputText>
+            <InputText
               className="input-select"
               name="gender"
               style={{ top: "68%" }}
-              onChange={handleChange}
-              value={state.value}
+              value={gender}
+              disabled="true"
             >
-              <option value="none">None</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
+            </InputText>
           </div>
           <div className="row-cols-6">
             <label className="rectangle"></label>
@@ -125,13 +151,13 @@ function ShowPost() {
               className="description text-black"
               placeholder="Need a high school math teacher."
               name="description"
-              onChange={handleChange}
+              value={post.description}
+              disabled="true"
             />
           </div>
           <div className="row-cols-6">
             <Button
               className="button-contact position-abs text-white"
-              onClick={handleSubmit}
             >
               Contact right now!
             </Button>
