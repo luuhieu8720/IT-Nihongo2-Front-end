46bb3745a14e5e1128d69d5adce62278c916cb63
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-25 19:46:47 +0700

    update some changes in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,36 @@
 import { Link } from "react-router-dom";
 import SidebarHomePage from "../components/Homepage/SidebarHomePage";
-import { Card } from "react-bootstrap";
+import PostServices from "../services/PostServices"
 import TopComponent from "../components/Homepage/TopComponent";
 import StudentSideBar from "../components/StudentSideBar";
+import Thumbnail from "../components/Homepage/Thumbnail";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+    var postIDs = []
+    const [posts, setPostIDs] = useState([])
+    useEffect(
+        () => {
+            PostServices.getAllPost().then( response => {
+                var listPosts = response.data.value; 
+                let ids = listPosts.map(post => post.id);
+                setPostIDs(ids); 
+                console.log(ids)
+            })
+            .catch(error => console.log(error));
+        }, []
+    )
+
+    Object.values(posts).forEach((id) => {
+        postIDs.push(id);
+    })
+
+    const listPosts = postIDs.map((postID) =>
+        <div>
+            <Thumbnail id={postID} ></Thumbnail>
+            <div className="blank"></div>
+        </div>
+    )
     return (
         <div className="row">
             <div className="col-sm-1" >
@@ -15,87 +41,7 @@ function HomePage() {
                     <TopComponent />
                 </div>
                 <div style={{ marginTop: '30px' }}>
-                    <Card className="card-tutor">
-                        <Card.Body>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Card.Title>Title: Finding math teacher</Card.Title>
-                                    <Card.Text>Description: Need a high school math teacher</Card.Text>
-                                    <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="dropdown">
-                                        <i className="fa fa-ellipsis-v" style={{ marginLeft: '95%' }} type="" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </i>
-                                        <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
-                                            <li><Link className="dropdown-item" to="/" >View detail</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Report this post</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Manage</Link></li>
-                                        </ul>
-                                    </div>
-                                    <Card.Subtitle>Salary: 300$</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
-
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-tutor">
-                        <Card.Body>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Card.Title>Title: Finding math teacher</Card.Title>
-                                    <Card.Text>Description: Need a high school math teacher</Card.Text>
-                                    <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="dropdown">
-                                        <i className="fa fa-ellipsis-v" style={{ marginLeft: '95%' }} type="" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </i>
-                                        <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
-                                            <li><Link className="dropdown-item" to="/" >View detail</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Report this post</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Manage</Link></li>
-                                        </ul>
-                                    </div>
-                                    <Card.Subtitle>Salary: 300$</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
-
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-tutor">
-                        <Card.Body>
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <Card.Title>Title: Finding math teacher</Card.Title>
-                                    <Card.Text>Description: Need a high school math teacher</Card.Text>
-                                    <Card.Text className="position-abs text-time">September, 16 - September, 30</Card.Text>
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="dropdown">
-                                        <i className="fa fa-ellipsis-v" style={{ marginLeft: '95%' }} type="" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                                        </i>
-                                        <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
-                                            <li><Link className="dropdown-item" to="/" >View detail</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Report this post</Link></li>
-                                            <li><Link className="dropdown-item" to="/" >Manage</Link></li>
-                                        </ul>
-                                    </div>
-                                    <Card.Subtitle>Salary: 300$</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Time: 17:00 - 19:00 Mon, Wed, Sun</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Location: Danang</Card.Subtitle>
-                                    <Card.Subtitle className="mt-2">Gender: None</Card.Subtitle>
-
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
+                    {listPosts}
                 </div>
             </div>
             <div className="col-sm-3 student-top-component" style={{ paddingLeft: '5%' }}>
