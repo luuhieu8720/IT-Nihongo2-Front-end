6aa3738432e75800c254ec1dcf3e6d769d870e5b
luuhieu8720 <lethiluuhieu@gmail.com>
2021-10-28 21:40:36 +0700

    Add full features for homepage and fix profile setting

--- src/pages/ShowPost.jsx
+++ src/pages/ShowPost.jsx
@@ -43,7 +43,7 @@ function ShowPost({ match }) {
 						startMinus: tmp.time[0].startMinus,
 						endMinus: tmp.time[0].endMinus,
 					},
-					location: tmp.location,
+					location: `${tmp.ward}, ${tmp.district}, ${tmp.city}`,
 					male: tmp.male,
 					details: tmp.details,
 					salary: tmp.salary,
@@ -67,96 +67,99 @@ function ShowPost({ match }) {
 	}, [])
 
 	return (
-		<div className="background1">
-			<ToastContainer />
+		<div>
+			<div className="background1">
+				<ToastContainer />
+				
+			</div>
 			<div className="frame-show position-abs">
-				<div className="col-6">
-					<div className="row-cols-6">
-						<label className="tutor-asking position-abs">TUTOR-ASKING</label>
-						<p className="title">Title</p>
-						<p className="salary">Salary</p>
-						<p className="time">Time</p>
-						<p className="location">Location</p>
-						<p className="gender">Gender</p>
-					</div>
-					<div className="row-cols-6">
-						<InputText
-							className="input-showpost text-black position-abs"
-							placeholder="Finding math teacher..."
-							name="title"
-							value={post.title}
-							style={{ left: "6.5%" }}
-							disabled="true"
-						/>
-						<InputText
-							className="input-showpost text-black position-abs"
-							name="salary"
-							value={post.salary}
-							style={{ left: "49.5%" }}
-							disabled="true"
-						/>
-						<p className="special">/</p>
-						<InputText
-							className="input-showpost text-black text-center position-abs"
-							placeholder="9:00-11:00"
-							name="time"
-							value={post.time.startHour + ":" + post.time.startMinus + "-"
-								+ post.time.endHour + ":" + post.time.endMinus}
-							style={{ left: "49.5%", top: "35%", width: "13%" }}
-							disabled="true"
-						/>
-						<InputText
-							className="input-showpost text-black position-abs"
-							name="day"
-							style={{ left: "66%", top: "35%", width: "21.6%" }}
-							disabled="true"
-							value={post.day}
-						/>
-						<p className="money">$</p>
-					</div>
-					<div className="row-cols-6">
-						<InputText
-							className="input-select"
-							name="location"
-							style={{ top: "52%" }}
-							value={post.location}
-							disabled="true"
-						>
-						</InputText>
-						<InputText
-							className="input-select"
-							name="gender"
-							style={{ top: "68%" }}
-							value={post.male == true ? "Male" : "Female"}
-							disabled="true"
-						>
-						</InputText>
-					</div>
-					<div className="row-cols-6">
-						<label className="rectangle"></label>
-						<p
-							className="enter-your-description"
-							name="description"
-						>
-							Post details
-						</p>
-						<textarea
-							className="description text-black"
-							placeholder="Need a high school math teacher."
-							name="description"
-							value={post.details}
-							disabled="true"
-						/>
-					</div>
-					<div className="row-cols-6">
-						<Button
-							className="button-contact position-abs text-white"
-						>
-							Contact right now!
-						</Button>
+					<div className="col-6">
+						<div className="row-cols-6">
+							<label className="tutor-asking position-abs">TUTOR-ASKING</label>
+							<p className="title">Title</p>
+							<p className="salary">Salary</p>
+							<p className="time">Time</p>
+							<p className="location">Location</p>
+							<p className="gender">Gender</p>
+						</div>
+						<div className="row-cols-6">
+							<InputText
+								className="input-showpost text-black position-abs"
+								placeholder="Finding math teacher..."
+								name="title"
+								value={post.title}
+								style={{ left: "6.5%" }}
+								disabled="true"
+							/>
+							<InputText
+								className="input-showpost text-black position-abs"
+								name="salary"
+								value={post.salary}
+								style={{ left: "49.5%" }}
+								disabled="true"
+							/>
+							<p className="special">/</p>
+							<InputText
+								className="input-showpost text-black text-center position-abs"
+								placeholder="9:00-11:00"
+								name="time"
+								value={post.time.startHour + ":" + post.time.startMinus + "-"
+									+ post.time.endHour + ":" + post.time.endMinus}
+								style={{ left: "49.5%", top: "35%", width: "13%" }}
+								disabled="true"
+							/>
+							<InputText
+								className="input-showpost text-black position-abs"
+								name="day"
+								style={{ left: "66%", top: "35%", width: "21.6%" }}
+								disabled="true"
+								value={post.day}
+							/>
+							<p className="money">$</p>
+						</div>
+						<div className="row-cols-6">
+							<InputText
+								className="input-select"
+								name="location"
+								style={{ top: "52%" }}
+								value={post.location}
+								disabled="true"
+							>
+							</InputText>
+							<InputText
+								className="input-select"
+								name="gender"
+								style={{ top: "68%" }}
+								value={post.male == true ? "Male" : "Female"}
+								disabled="true"
+							>
+							</InputText>
+						</div>
+						<div className="row-cols-6">
+							<label className="rectangle"></label>
+							<p
+								className="enter-your-description"
+								name="description"
+							>
+								Post details
+							</p>
+							<textarea
+								className="description text-black"
+								placeholder="Need a high school math teacher."
+								name="description"
+								value={post.details}
+								disabled="true"
+							/>
+						</div>
+						<div className="row-cols-6">
+							<Button
+								className="button-contact position-abs text-white"
+							>
+								Contact right now!
+							</Button>
+						</div>
 					</div>
 				</div>
-			</div>
 		</div>
 	);
 }
