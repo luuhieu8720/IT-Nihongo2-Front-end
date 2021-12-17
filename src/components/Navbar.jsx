3cc05378713c315a7d577da94cd7f4450e4b197e
luuhieu8720 <lethiluuhieu@gmail.com>
2021-12-17 09:57:24 +0700

    Update and fix some rendering bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
 import UserServices from "../services/UserServices";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import SignOut from "../logics/SignOut";
 
 function Navbar() {
     const [user, setUser] = useState({
@@ -12,8 +14,19 @@ function Navbar() {
         email: "",
         avatar: "",
     });
+
+    const signOut = () => {
+        toast.success("Successfully");
+        setTimeout(() => {
+            SignOut.signOut();
+            history.push("/signin");
+        }, 3000);
+    };
+
     const [currentUser, setCurrentUser] = useState({ role: "", specialty: "" });
+    
     const history = useHistory();
+    
     useEffect(() => {
         UserServices.getUserInformation().then((response) => {
             setUser(response.data.value);
@@ -22,6 +35,7 @@ function Navbar() {
     }, []);
     return (
         <div className="row">
+            <ToastContainer />
             <div className="col-sm-auto student-top-component" style={{ marginLeft: '3%' }}>
                 <i className="far fa-envelope fa-2x"></i>
             </div>
@@ -29,20 +43,19 @@ function Navbar() {
                 <i className="far fa-bell fa-2x"></i>
             </div>
             <div className="col-sm-auto student-top-component" style={{ paddingRight: '-1%', paddingLeft: '5%' }} >
-                <p >Hi, {localStorage.getItem('currentUser') == null ? history.push("/signin") : user.name}</p>
+                <p >Hi, {localStorage.getItem('currentUser') == null ? history.push("/signin") : user.username}</p>
 
             </div>
             <div className="col-sm-auto" style={{ marginTop: '-20%' }}>
                 <div className="dropdown">
-                    <Image className="position-abs" src={user.avatar == "" || user.avatar == null ? "/Image/avatardefault.png" : user.avatar} style={{ marginTop:"50px" }}
+                    <Image className="position-abs" src={user.avatar == "" || user.avatar == null ? "/Image/avatardefault.png" : user.avatar} style={{ marginTop: "50px" }}
                         width="60" height="60" alt="image" id="dropdownMenuButton2"
                         data-bs-toggle="dropdown" aria-expanded="false" roundedCircle
                     ></Image>
                     <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="dropdownMenuButton2">
                         <li><Link className="dropdown-item"
                             to={currentUser.role == "User" ? "/user/profile/setting" : "/"} onClick={() => { sessionStorage.setItem("stateTabIndex", "1") }}>Profile</Link></li>
-                        <li><Link className="dropdown-item" to="/" >Profile</Link></li>
-                        <li><Link className="dropdown-item" to="/" >Sign out</Link></li>
+                        <li><Link className="dropdown-item" to="/signin" onClick={signOut} >Sign out</Link></li>
                     </ul>
                 </div>
             </div>
