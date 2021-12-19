9e44a07d1336aff2b15a42bd134efca26cd5ae74
luuhieu8720 <lethiluuhieu@gmail.com>
2021-12-19 09:24:05 +0700

    Fix tutor-list bugs

--- src/components/Tutor/TopTutorComponent.jsx
+++ src/components/Tutor/TopTutorComponent.jsx
@@ -2,7 +2,7 @@ function TopTutorComponent() {
   return (
     <div className="row mt-4 ms-8" style={{ marginBottom: "0%" }}>
       <div className="col-sm-6">
-        <div className="text-tutor-list ms-4">WE HAVE MANY EXCELLENT TUTOR</div>
+        <div className="text-tutor-list ms-4">WE HAVE MANY EXCELLENT TUTORS</div>
         <div
           className="ms-6 text-tutor-list1"
           style={{ marginTop: "1%", marginLeft: "10%" }}
